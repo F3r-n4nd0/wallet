4637620cabf5a83dd6ec996b58701177a56e1ceb
Fernando Luna Molina <337n4nd0@gmail.com>
2015-06-04 20:43:24 -0400

    Initial Commit

---
+++ Wallet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wallet
+//
+//  Created by Fernando on 6/4/15.
+//  Copyright (c) 2015 F3rn4nd0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
